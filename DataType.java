1a99e03ffce1f146e20e8c6ae87cf87c9b77a6ad
yasin-arafat02 <arn.shibloo02@gmail.com>
2025-07-24 14:45:17 +0600

    add

---
+++ DataType.java
@@ -0,0 +1,14 @@
+public class DataType {
+    public static void main(String[] args) {
+        int age = 25;
+        double pi = 3.1416;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Shibloo";
+
+         System.out.println(name + " is " + age + " years old.");
+        System.out.println("Grade: " + grade);
+        System.out.println("Pi value: " + pi);
+        System.out.println("Is student? " + isStudent);
+    }
+}
